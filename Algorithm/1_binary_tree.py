91b4cc56273fd2ccf8d9914b7106d41740db4b0e
esaul <1391esaul@gmail.com>
2023-10-25 22:20:42 +0300

    Asus

--- Algorithm/1_binary_tree.py
+++ Algorithm/1_binary_tree.py
@@ -1,21 +1,23 @@
 # example
 
-def binary_search(list, item):
-    low = 0
-    high = len(list)-1
+def binary_search(lst, item):
+    low = 0                 # Нижний индекс массива
+    high = len(lst) - 1     # Верхний индекс массива
 
-    while low <= high:
-        mid = (low + high)//2
-        guess = list[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
+    while low <= high:      # Пока нижний индекс не превышает верхний
+        mid = (low + high) // 2   # Находим средний индекс (целочисленное деление)
+        guess = lst[mid]          # Получаем значение в середине массива
+
+        if guess == item:         # Если угадали
+            return mid            # Возвращаем индекс элемента
+        if guess > item:          # Если угаданное значение больше искомого
+            high = mid - 1        # Сужаем диапазон до левой половины
         else:
-            low = mid + 1
-    return None
+            low = mid + 1         # Иначе сужаем диапазон до правой половины
+
+    return None                   # Если элемент не найден, возвращаем None
 
 my_list = [1, 3, 5, 7, 9]
 
-print(binary_search(my_list, 3)) # => 1
-print(binary_search(my_list,5)) # => None
+print(binary_search(my_list, 3))  # => 1 (индекс числа 3 в массиве)
+print(binary_search(my_list, -1)) # => None (число -1 не найдено в массиве)
