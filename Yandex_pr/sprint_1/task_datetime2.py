6fa4e75440f52e93142d1ffa261d4ea62d8770cd
esaul <1391esaul@gmail.com>
2023-10-28 08:03:01 +0300

    A

--- Yandex_pr/sprint_1/task_datetime2.py
+++ Yandex_pr/sprint_1/task_datetime2.py
@@ -1,21 +1,27 @@
 import datetime as dt
 
+FORMAT = '%d.%m.%Y'
 
-def get_days_to_birthda(date_birthday):
+
+def get_days_to_birthda(name, date_birthday):
+    date_birthday = dt.datetime.strptime(date_birthday, FORMAT)
+    date_birthday = date_birthday.date()
     today = dt.date.today()
     today_year = today.year
     date_birthday = date_birthday.replace(year=today_year)
 
     if today > date_birthday:
-        date_birthday = date_birthday.replace(year=today_year+1)
-        days_left = date_birthday - today
-    else:
-        days_left = date_birthday - today
-    return days_left
+        date_birthday = date_birthday.replace(year=today_year + 1)
+
+    days_left = date_birthday - today
+    return f'{name}            {days_left}'
 
 
-viktor_birthday = dt.date(2015, 7, 9)
-lev_birthday = dt.date(2021, 8, 5)
+birthday = {
+    ('lera', '16.05.2015'),
+    ('max', '16.12.2011'),
+    ('toly', '12.06.2016')
+}
 
-print(get_days_to_birthda(viktor_birthday))
-print(get_days_to_birthda(lev_birthday))
+for dict in birthday:
+    print(get_days_to_birthda(dict[0], dict[1]))
