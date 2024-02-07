752e1152d470c2ce7c5997e26ee274d1aa2430d0
esaul <1391esaul@gmail.com>
2024-02-07 07:00:10 +0300

    A

---
+++ Stepik_ООП/7/7_3.py
@@ -0,0 +1,25 @@
+class LowerString(str):
+    def __new__(cls, obj):
+        isintace = super().__new__(cls, obj)
+        isintace.obj = obj.lower()
+        return isintace
+s1 = LowerString('BEEGEEK')
+s2 = LowerString('BeeGeek')
+
+print(s1)
+print(s2)
+print(s1 == s2)
+print(issubclass(LowerString, str))
+
+
+# class UpperPrintString(str):
+#     def __str__(self):
+#         return f'{super().__str__().upper()}'
+#
+#
+#
+# s1 = UpperPrintString('beegeek')
+# s2 = UpperPrintString('BeeGeek')
+#
+# print(s1)
+# print(s2)
