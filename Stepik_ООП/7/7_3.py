dbc9cad56156aa9b5e3ce166605df9eeb1a86547
esaul <1391esaul@gmail.com>
2024-02-08 07:04:59 +0300

    A

--- Stepik_ООП/7/7_3.py
+++ Stepik_ООП/7/7_3.py
@@ -1,15 +1,25 @@
-class LowerString(str):
-    def __new__(cls, obj):
-        isintace = super().__new__(cls, obj)
-        isintace.obj = obj.lower()
-        return isintace
-s1 = LowerString('BEEGEEK')
-s2 = LowerString('BeeGeek')
+class RoundedInt(int):
+    def __new__(cls, num, even=True):
+        if even:
+            num = (num + 1) // 2 * 2
+        else:
+            num = (num + 1) // 2 * 2 - 1
+        return super().__new__(cls, num)
 
-print(s1)
-print(s2)
-print(s1 == s2)
-print(issubclass(LowerString, str))
+
+
+# class LowerString(str):
+#     def __new__(cls, obj):
+#         isintace = super().__new__(cls, obj)
+#         isintace.obj = obj.lower()
+#         return isintace
+# s1 = LowerString('BEEGEEK')
+# s2 = LowerString('BeeGeek')
+#
+# print(s1)
+# print(s2)
+# print(s1 == s2)
+# print(issubclass(LowerString, str))
 
 
 # class UpperPrintString(str):
