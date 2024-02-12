fd5e58cb7c2fffab7d6530f96a5974af778d1131
esaul <1391esaul@gmail.com>
2024-02-12 07:07:47 +0300

    A

--- Stepik_ООП/7/7_2.py
+++ Stepik_ООП/7/7_2.py
@@ -1,24 +1,91 @@
-class Summator:
-    def __init__(self, m=1):
-        self.m = m
+class USADate:
+    def __init__(self, year, month, day):
+        self.year = year
+        self.month = month
+        self.day = day
 
-    def total(self, n):
-        return sum(map(lambda x: x ** self.m, range(1, n + 1)))
+    def format(self):
+        return f'{self.month}-{self.day}-{self.year}'
 
-
-class SquareSummator(Summator):
-    def __init__(self):
-        super().__init__(2)
-
-
-class QubeSummator(Summator):
-    def __init__(self):
-        super().__init__(3)
+    def iso_format(self):
+        return f"{self.year}-{self.month}-{self.day}"
 
 
-class CustomSummator(Summator):
-    def __init__(self, m):
-        super().__init__(m)
+
+
+
+
+
+
+
+# res = {}
+# for num in numbers:
+#     if num not in res:
+#         res[num] = 1
+#     else:
+#         res[num] += 1
+#
+# item = info.get('key', default=None)  возвращ значение по умолчанию не меняя словаря
+# info1.update(info2)   info1 |= info2
+# dict.setdefault(key, default=None)  если ключ отсутсвует вставляет значение изменяя словарь
+# def info['job']
+# job = info.pop('job', None)
+# job = info.popitem()  удалит последний элемент словаря
+# description = {1 : 'ONE', 2 : 'TWO', 3 : 'TREE'}
+# print(description.get(num, 'Unknown'))
+#
+
+
+
+
+
+
+
+
+# s = input().split(' ')
+# d = {}
+# sp = []
+# for i in s:
+#     d[i] = d.setdefault(i, 0) + 1
+#     sp.append(d[i])
+# print(*sp)
+
+
+# def build_query_string(params):
+#     # Сортируем ключи словаря
+#     sorted_keys = sorted(params.keys())
+#
+#     # Формируем список строк вида "ключ=значение"
+#     query_list = [f"{key}={params[key]}" for key in sorted_keys]
+#
+#     # Объединяем список строк с помощью символа "&"
+#     query_string = "&".join(query_list)
+#
+#     return query_string
+#
+# print(build_query_string({'name': 'timur', 'age': 28}))
+# print(build_query_string({'sport': 'hockey', 'game': 2, 'time': 17}))
+
+#     def __init__(self, m=1):
+#         self.m = m
+#
+#     def total(self, n):
+#         return sum(map(lambda x: x ** self.m, range(1, n + 1)))
+#
+#
+# class SquareSummator(Summator):
+#     def __init__(self):
+#         super().__init__(2)
+#
+#
+# class QubeSummator(Summator):
+#     def __init__(self):
+#         super().__init__(3)
+#
+#
+# class CustomSummator(Summator):
+#     def __init__(self, m):
+#         super().__init__(m)
 
 # class Counter:
 #     def __init__(self, start=0):
